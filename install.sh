aece22c3eccb3cb13ce63d92d44c82d32c159b36
The Stranjer <791672+TheStranjer@users.noreply.github.com>
2023-08-31 00:02:27 -0600

    Update install.sh
    Add 7zip

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 apt update
 apt upgrade -y
-apt install git git-lfs neovim neofetch whois postgresql postgresql-contrib tmux ffmpeg curl -y
+apt install git git-lfs neovim neofetch whois postgresql postgresql-contrib tmux ffmpeg curl p7zip-full -y
